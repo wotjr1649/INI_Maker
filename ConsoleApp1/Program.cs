03b72070132ed6a78d766666c579b7c1d72112ef
Jaeseok <wotjr1649@gmail.com>
2022-11-21 02:32:43 +0900

    cmt

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lst = new List<string>{ "H", "J" ,"a", "e"};
+            IniFile ini = new IniFile();
+            for(int i= 0; i < lst.Count; i++)
+            {
+                ini["Label"][i.ToString()] = $"{lst[i]}";
+            }
+            ini.Save("./Setting.ini");
+
+            ini.Load("./Setting.ini");
+
+            foreach (var i in ini["Label"])
+                Console.WriteLine(i.Key + i.Value);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
